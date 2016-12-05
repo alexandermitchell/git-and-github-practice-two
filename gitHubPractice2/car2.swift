9cb9f61cb786778f659d173d6b47bf63fb2fc51e
kobst <edwrdhn@gmail.com>
2016-12-05 16:46:07 -0500

    adding car class first time again

---
+++ gitHubPractice2/car2.swift
@@ -0,0 +1,29 @@
+//
+//  car2.swift
+//  gitHubPractice2
+//
+//  Created by Edward Han on 12/5/16.
+//  Copyright © 2016 Edward Han. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+    
+     var make: String
+    var model: String
+    var color: String
+    init(make: String, model: String, color: String) {
+                self.make = make
+                    self.model = model
+                    self.color = color
+            
+            }
+    
+    }
+
+
+class Garage {
+        var spaces: Int
+    
+}
